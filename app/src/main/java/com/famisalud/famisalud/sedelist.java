1ec2dcbe52672ef7751c5c9fa8309333a12051a3
Nestorpq <128406378+NestorPQ@users.noreply.github.com>
2023-10-28 06:14:03 -0500

    Cuadros de confirmación tipo SweetAlert Agregados para android

--- app/src/main/java/com/famisalud/famisalud/sedelist.java
+++ app/src/main/java/com/famisalud/famisalud/sedelist.java
@@ -6,8 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.famisalud.famisalud.Adapter.MyApapterSede;
+import com.famisalud.famisalud.Model.SedeClass;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -16,6 +21,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 public class sedelist extends AppCompatActivity {
 
    RecyclerView recyclerView;
@@ -23,19 +30,29 @@ public class sedelist extends AppCompatActivity {
    MyApapterSede myApapterSede;
    ArrayList<SedeClass> list;
 
+   Button btdialog;
+
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_sedelist);
 
+//      btdialog = findViewById(R.id.btndialogS);
+//      btdialog.setOnApplyWindowInsetsListener(new View.OnClickListener() {
+//         @Override
+//         public void onClick(View v) {
+//            SweetAlertDialog pDialog = new SweetAlertDialog(con, SweetAlertDialog.PROGRESS_TYPE);
+//            pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+//            pDialog.setTitleText("Loading");
+//            pDialog.setCancelable(false);
+//            pDialog.show();
+//         }
+//      });
+
       recyclerView = findViewById(R.id.sedelist);
       database = FirebaseDatabase.getInstance().getReference("sedes");
       recyclerView.setHasFixedSize(true);
-      recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
-
-      // Agregar un espaciado entre elementos (ajusta el valor de spacing según tus necesidades)
-//      int spacing = getResources().getDimensionPixelSize(R.dimen.spacing_between_cardviews);
-//      recyclerView.addItemDecoration(new ItemSpacingDecoration(spacing));
+      recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
       list = new ArrayList<>();
       myApapterSede = new MyApapterSede(this, list);
--- app/src/main/java/com/famisalud/famisalud/ui/home/HomeFragment.java
+++ app/src/main/java/com/famisalud/famisalud/ui/home/HomeFragment.java
@@ -1,110 +1,23 @@
-//package com.famisalud.famisalud.ui.home;
-//
-//import android.os.Bundle;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.LinearLayout;
-//import android.widget.TextView;
-//
-//import androidx.annotation.NonNull;
-//import androidx.annotation.Nullable;
-//import androidx.cardview.widget.CardView;
-//import androidx.fragment.app.Fragment;
-//import androidx.lifecycle.ViewModelProvider;
-//
-//import com.denzcoskun.imageslider.ImageSlider;
-//import com.denzcoskun.imageslider.models.SlideModel;
-//import com.famisalud.famisalud.R;
-//import com.famisalud.famisalud.databinding.FragmentHomeBinding;
-//
-//import java.util.List;
-//
-//public class HomeFragment extends Fragment {
-//
-//   private HomeViewModel viewModel;
-//   private FragmentHomeBinding binding;
-//
-//   public View onCreateView(@NonNull LayoutInflater inflater,
-//                            ViewGroup container, Bundle savedInstanceState) {
-//      HomeViewModel homeViewModel =
-//          new ViewModelProvider(this).get(HomeViewModel.class);
-//
-//      binding = FragmentHomeBinding.inflate(inflater, container, false);
-//      View root = binding.getRoot();
-//
-//      final TextView textView = binding.textHome;
-//      homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-//
-//      //viewModel = new HomeViewModel();
-//      setupCardViews();
-//      setupUI();
-//      return root;
-//   }
-//
-//   private void setupCardViews() {
-//      // Obtén una referencia al LinearLayout que contiene los CardViews
-//      LinearLayout cardContainer = root.findViewById(R.id.cardContainer);
-//
-//      // Crea y agrega tus CardViews
-//      for (int i = 0; i < 5; i++) { // Ejemplo: crea 5 CardViews
-//         CardView cardView = new CardView(requireContext());
-//         cardView.setLayoutParams(new LinearLayout.LayoutParams(
-//             getResources().getDimensionPixelSize(R.dimen.card_width),
-//             getResources().getDimensionPixelSize(R.dimen.card_height)
-//         ));
-//
-//         // Configura el contenido de tu CardView (puedes personalizarlo según tus necesidades)
-//         LayoutInflater inflater = LayoutInflater.from(requireContext());
-//         View cardContent = inflater.inflate(R.layout.card_content, null);
-//         cardView.addView(cardContent);
-//
-//         // Agrega el CardView al LinearLayout
-//         cardContainer.addView(cardView);
-//      }
-//   }
-//
-//   private void setupUI() {
-//      ImageSlider imageSlider = binding.imageSlider;
-//
-//      HomeViewModel viewModel = new ViewModelProvider(this).get(HomeViewModel.class);
-//      List<SlideModel> slideModelsList = viewModel.getSlideModelsList();
-//
-//      // Configura el ImageSlider
-//      imageSlider.setImageList(slideModelsList);
-//   }
-//
-//   @Override
-//   public void onDestroyView() {
-//      super.onDestroyView();
-//      binding = null;
-//   }
-//}
-
-
 package com.famisalud.famisalud.ui.home;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.models.SlideModel;
-import com.famisalud.famisalud.R;
 import com.famisalud.famisalud.databinding.FragmentHomeBinding;
 import com.famisalud.famisalud.sedelist;
 
 import java.util.List;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class HomeFragment extends Fragment {
 
@@ -123,42 +36,49 @@ public class HomeFragment extends Fragment {
       homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
       //viewModel = new HomeViewModel();
-      setupCardViews(root); // Pasa la vista 'root' como argumento
+//      setupCardViews(root); // Pasa la vista 'root' como argumento
       setupUI();
+
+      binding.btnDialog.setOnClickListener(v -> {
+
+         SweetAlertDialog dialog = new SweetAlertDialog(requireActivity(), SweetAlertDialog.SUCCESS_TYPE);
+         dialog.setTitleText("Exitoso");
+         dialog.show();
+      });
 
       binding.btnIrReciclerView.setOnClickListener(v -> startActivity(new Intent(requireContext(), sedelist.class)));
 
       return root;
    }
 
-   private void setupCardViews(View root) {
-      // Obtén una referencia al LinearLayout que contiene los CardViews
-      LinearLayout cardContainer = root.findViewById(R.id.cardContainer);
-
-      // Crea y agrega tus CardViews
-      for (int i = 0; i < 7; i++) { // Ejemplo: crea 7 CardViews
-         CardView cardView = new CardView(requireContext());
-
-         LinearLayout.LayoutParams cardLayoutParams = new LinearLayout.LayoutParams(
-             getResources().getDimensionPixelSize(R.dimen.card_width),
-             getResources().getDimensionPixelSize(R.dimen.card_height)
-         );
-
-         // Agrega márgenes izquierdo y derecho al CardView para separarlos
-         cardLayoutParams.setMarginStart(getResources().getDimensionPixelSize(R.dimen.card_margin));
-         cardLayoutParams.setMarginEnd(getResources().getDimensionPixelSize(R.dimen.card_margin));
-
-         cardView.setLayoutParams(cardLayoutParams);
-
-         // Configura el contenido de tu CardView (puedes personalizarlo según tus necesidades)
-         LayoutInflater inflater = LayoutInflater.from(requireContext());
-         View cardContent = inflater.inflate(R.layout.cardview, null);
-         cardView.addView(cardContent);
-
-         // Agrega el CardView al LinearLayout
-         cardContainer.addView(cardView);
-      }
-   }
+//   private void setupCardViews(View root) {
+//      // Obtén una referencia al LinearLayout que contiene los CardViews
+//      LinearLayout cardContainer = root.findViewById(R.id.cardContainer);
+//
+//      // Crea y agrega tus CardViews
+//      for (int i = 0; i < 7; i++) { // Ejemplo: crea 7 CardViews
+//         CardView cardView = new CardView(requireContext());
+//
+//         LinearLayout.LayoutParams cardLayoutParams = new LinearLayout.LayoutParams(
+//             getResources().getDimensionPixelSize(R.dimen.card_width),
+//             getResources().getDimensionPixelSize(R.dimen.card_height)
+//         );
+//
+//         // Agrega márgenes izquierdo y derecho al CardView para separarlos
+//         cardLayoutParams.setMarginStart(getResources().getDimensionPixelSize(R.dimen.card_margin));
+//         cardLayoutParams.setMarginEnd(getResources().getDimensionPixelSize(R.dimen.card_margin));
+//
+//         cardView.setLayoutParams(cardLayoutParams);
+//
+//         // Configura el contenido de tu CardView (puedes personalizarlo según tus necesidades)
+//         LayoutInflater inflater = LayoutInflater.from(requireContext());
+//         View cardContent = inflater.inflate(R.layout.cardview, null);
+//         cardView.addView(cardContent);
+//
+//         // Agrega el CardView al LinearLayout
+//         cardContainer.addView(cardView);
+//      }
+//   }
 
 
    private void setupUI() {
